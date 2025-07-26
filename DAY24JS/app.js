c1f148467b39ccc5e29bbaefc6dd4872b408a18e
Syed Farhad Al Mubid <syedfarhadalmubid@gmail.com>
2025-07-26 23:26:56 +0530

    Day 24: Started JavaScript – Variables, Scope, Data Types

---
+++ DAY24JS/app.js
@@ -0,0 +1,22 @@
+console.log("hello world this is javascript");
+
+let a = 50; //variable declaration and initialization
+let b = 60;
+
+console.log(a+b);  //adding the values of 2 variables 
+
+console.log(50+50+100);
+
+const pi = 3.14;  //its a constant which value remain same 
+
+console.log(pi);
+
+let age = 25;  //global variable 
+
+console.log(age); //accessing global variable
+
+{
+    console.log(age);  //accessing block level variable
+}
+
+
