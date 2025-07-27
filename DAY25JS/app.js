4a207e0137930ffb1cc6085c70572d3bcd4485e5
Syed Farhad Al Mubid <syedfarhadalmubid@gmail.com>
2025-07-27 23:18:00 +0530

    Day 25 - Practiced data types, operators, truthy/falsy values, typeof, NaN and more in JavaScript

---
+++ DAY25JS/app.js
@@ -0,0 +1,23 @@
+let a = "farhad";
+let b = 3;
+console.log(a-b); //output will be NaN
+
+let num = 11; //assignment operator
+let num2 = 2;
+console.log(num%num2); //modulus operator gives remainder
+
+console.log(2**5); //exponentiation operator output will be 32
+
+console.log(13=="13"); //output will be true it only checks value not data type
+console.log(13==="13"); //output will be false it checks both value and data type
+
+console.log(12!=12); //output will be false
+console.log(12!=="12") ; //output will be true 
+
+console.log(1<2); //true
+console.log(1>2 && 2>5); //output will be false because both the conditions are false
+console.log(5>3 || 2<1); //output will be true
+
+2>1 ? console.log("Its true") : console.log("its false"); //condition is true so
+//the part before colon will run
+
