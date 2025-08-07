614e918a6511e8e6f89a69af99ac91d9e50d343b
Syed Farhad Al Mubid <syedfarhadalmubid@gmail.com>
2025-08-07 23:48:19 +0530

    Day 36 - Today I practiced DOM

---
+++ DAY36 DOM/app.js
@@ -0,0 +1,35 @@
+// //select element using get element by Id
+// console.log(document.getElementById("main-heading")); 
+
+// //Query Selector
+// const logo = document.querySelector(".logo");
+// console.log(logo);
+
+// const navitem = document.querySelectorAll(".nav-item");
+// console.log(navitem);
+
+// //changing a text
+
+// const main = document.getElementById("main-heading");
+// console.log(main);
+// console.log(main.textContent);
+// main.textContent = "This is new text";
+
+// //changing the style of an element
+
+// main.style.color = "blue";
+// main.style.border = "2px solid red";
+
+//get multiple elements using getelement by class
+
+// const navitems = document.getElementsByClassName("nav-item");
+// console.log(navitems[0]);
+
+// navitems[0].style.color = "brown";
+
+
+//get element by class
+const link = document.getElementsByClassName("links");
+console.log(link);
+
+link[0].style.color = "brown";
